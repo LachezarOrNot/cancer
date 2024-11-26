9422854ae054a5ba2a527fd016d4a7852e685562
Screw_Driver <120485741+LachezarOrNot@users.noreply.github.com>
2024-11-27 01:46:41 +0930

    Add files via upload

---
+++ ViewModels/DeviceViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EffiSenseRe.ViewModels {
+    public class DeviceViewModel {
+        public string Name { get; set; }
+        public double AnnualEnergyConsumption { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ ViewModels/HomeWithDevicesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EffiSenseRe.ViewModels {
+    public class HomeWithDevicesViewModel {
+        public string Address { get; set; }
+        public int Size { get; set; }
+        public int RoomsCount { get; set; }
+        public string HeatingType { get; set; }
+        public List<DeviceViewModel> Devices { get; set; }
+    }
+}
